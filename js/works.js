64eddb75c49725d0c3bc55a83cdca466a875e09a
shotaro314 <85724397+shotaro314@users.noreply.github.com>
2021-10-21 11:40:40 +0900

    xml test 1

---
+++ js/works.js
@@ -0,0 +1,24 @@
+$(function() {
+	$.ajax({
+		url: '../work_config.xml',
+		dataType: 'xml',
+		success: function(data) {
+
+			var insertContents = '';
+			$('item', data).each(function() {
+				var thisItem = $(this);
+        			insertContents += '<li>';
+					insertContents += '<status>';
+					insertContents += thisItem.children('status').text();
+					insertContents += '</status>';
+					insertContents += '<p>';
+					insertContents += '<a href="' + thisItem.children('link').text() + '" target="_blank">';
+					insertContents += thisItem.children('name').text();
+					insertContents += '</a>';
+					insertContents += '</p>';
+        			insertContents += '</li>';
+			});
+			$('.linkbox-news').append(insertContents);
+		}
+	});
+});
